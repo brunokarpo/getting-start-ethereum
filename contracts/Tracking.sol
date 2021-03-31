87ef945d5ddbdb888cc9da540002dccc1a0e0dca
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2021-03-30 22:36:16 -0300

    Added StoreVerification step Created address to operate the contract Added transition step to store verification Added verification in the next function to validate de transactions senders

--- contracts/Tracking.sol
+++ contracts/Tracking.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.5.10; //Versão do Solidity que será utilizada
 
 contract Tracking { // define que vamos criar um contrato chamado "Tracking"
 
-	enum Step { Supplier, SupplierToFactory, Factory, FactoryToStore }
+	enum Step { Supplier, SupplierToFactory, Factory, FactoryToStore, StoreVerification }
 
 	// Constantes que definem os status de cada etapa
 	// 'constant' define que é uma constante
@@ -19,11 +19,27 @@ contract Tracking { // define que vamos criar um contrato chamado "Tracking"
 	uint8 public supplierToFactoryState = Waiting;
 	uint8 public factoryState = Waiting;
 	uint8 public factoryToStoreState = Waiting;
+	uint8 public storeVerificationState = Waiting;
 
 	// Passo atual
 	// Vai mudar ao longo do tempo
 	// Inicia com o passo inicial
 	Step public currentStep = Step.Supplier;
+
+	// Roles
+	// representa endereços
+	address public supplier;
+	address public factory;
+	address public store;
+
+
+	// Construtor do contrato
+	constructor(address _supplier, address _factory, address _store) public {
+		supplier = _supplier;
+		factory = _factory;
+		store = _store;
+	}
+
 
 	// Método que será chamado, operará a máquina de status e realiza transações na blockchain.
 	// 'function' determina que o código será uma função
@@ -31,6 +47,11 @@ contract Tracking { // define que vamos criar um contrato chamado "Tracking"
 	// 'public' modificador de acesso que determina o nível de visibilidade da função
 	function next() public {
 		if (currentStep == Step.Supplier) { 					//Verifica o passo atual
+
+			if(msg.sender != supplier) {						// Verifica se quem está chamando o método next é o endereço do supplier
+				revert("User not allowed");						// Se não for, dá revert na transação, pois o usuário não pode alterar o status da operação
+			}
+
 			supplierState++; 									// incrementa o status do passo atual
 			if (supplierState == Finished) { 					// se o status do passo atual for finalizado
 				currentStep = Step.SupplierToFactory; 			// Troca para a proxima etapa
@@ -38,6 +59,10 @@ contract Tracking { // define que vamos criar um contrato chamado "Tracking"
 			return;												// Finaliza esse processo e pronto
 		}
 		if (currentStep == Step.SupplierToFactory) {
+			if(msg.sender != supplier) {						
+				revert("User not allowed");						
+			}
+
 			supplierToFactoryState++;
 			if (supplierToFactoryState == Finished) {
 				currentStep = Step.Factory;
@@ -45,6 +70,10 @@ contract Tracking { // define que vamos criar um contrato chamado "Tracking"
 			return;
 		}
 		if (currentStep == Step.Factory) {
+			if(msg.sender != factory) {						
+				revert("User not allowed");						
+			}
+
 			factoryState++;
 			if (factoryState == Finished) {
 				currentStep = Step.FactoryToStore;
@@ -52,15 +81,27 @@ contract Tracking { // define que vamos criar um contrato chamado "Tracking"
 			return;
 		}
 		if (currentStep == Step.FactoryToStore) {
-			if (factoryToStoreState == Finished) {
-				revert("Process is already finished");			// Finaliza o processo dando um erro para o usuário informando que não é possível trocar os status do step
+			if(msg.sender != factory) {						
+				revert("User not allowed");						
 			}
+
 			factoryToStoreState++;
+			if (factoryToStoreState == Finished) {
+				currentStep = Step.StoreVerification;
+			}
 			return;
 		}
+		if (currentStep == Step.StoreVerification) {
+			if(msg.sender != store) {						
+				revert("User not allowed");						
+			}
 
-
+			if (storeVerificationState == Finished) {
+				revert("Process is already finished");			// Finaliza o processo dando um erro para o usuário informando que não é possível trocar os status do step
+			}
+			storeVerificationState++;
+			return;
+		}
 	}
-
 
 }
