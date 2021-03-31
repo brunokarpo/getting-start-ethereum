53e4c72d3d6b75eed7955de61998a68eb0ec37a8
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2021-03-30 21:35:42 -0300

    Created the first contract with some variables

---
+++ contracts/Tracking.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.5.10; //Versão do Solidity que será utilizada
+
+contract Tracking { // define que vamos criar um contrato chamado "Tracking"
+
+	// Constantes que definem os status de cada etapa
+	// 'constant' define que é uma constante
+	// 'uint8' tipo de dado inteiro sem sinal com 8 bits (Unsigned Integer)
+	uint8 constant Waiting = 0;
+	uint8 constant Running = 1;
+	uint8 constant Finished = 2;
+
+	
+
+}
