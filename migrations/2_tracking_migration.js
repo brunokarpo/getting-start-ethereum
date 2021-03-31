c4cc577924111cb32238c3ae922b708b8036622d
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2021-03-30 22:42:24 -0300

    Changed migration file to receive constructor parameters

--- migrations/2_tracking_migration.js
+++ migrations/2_tracking_migration.js
@@ -1,5 +1,5 @@
 const Tracking = artifacts.require("Tracking");
 
 module.exports = function (deployer) {
-  deployer.deploy(Tracking);
+  deployer.deploy(Tracking, "0x2Af691753BF1467Cae6254063306FB3cca8a3aA5", "0x071895d0c9D125577A4385d51e3230d1fB13e908", "0x6d7001dD4FAB3915463133C8Cb23eF3c8561c69a");
 };
